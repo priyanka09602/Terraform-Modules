f3083c93317bf9bfab29d6de9dba278f68c99706
priyanka09602 <priyanka096.pradhan@gmail.com>
2024-08-06 20:44:50 +0530

    Update outputs.tf

--- Modules/blob/outputs.tf
+++ Modules/blob/outputs.tf
@@ -1,1 +1,20 @@
+output "storage_account_id" {
+  description = "The ID of the storage account."
+  value       = azurerm_storage_account.idc-hack-sa.id
+}
+
+output "storage_account_name" {
+  description = "The name of the storage account."
+  value       = azurerm_storage_account.idc-hack-sa.name
+}
+
+output "container_id" {
+  description = "The ID of the blob container."
+  value       = azurerm_storage_container.idc-hack-sc.id
+}
+
+output "container_name" {
+  description = "The name of the blob container."
+  value       = azurerm_storage_container.idc-hack-sc.name
+}
 
