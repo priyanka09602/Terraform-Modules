b28161035e08b2c9b84f3e52b83f63082d197f2f
priyanka09602 <priyanka096.pradhan@gmail.com>
2024-08-06 19:25:54 +0530

    Update variables.tf

--- Modules/blob/variables.tf
+++ Modules/blob/variables.tf
@@ -1,1 +1,40 @@
+variable "resource_group_name" {
+  description = "The name of the resource group."
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure region where resources will be created."
+  type        = string
+}
+
+variable "storage_account_name" {
+  description = "The name of the storage account."
+  type        = string
+}
+
+variable "account_tier" {
+  description = "The tier of the storage account (e.g., Standard, Premium)."
+  type        = string
+}
+
+variable "account_replication_type" {
+  description = "The replication type of the storage account (e.g., LRS, GRS, RA-GRS)."
+  type        = string
+}
+
+variable "container_name" {
+  description = "The name of the blob container."
+  type        = string
+}
+
+variable "container_access_type" {
+  description = "The access level of the blob container (e.g., private, blob, container)."
+  type        = string
+}
+
+variable "environment" {
+  description = "The environment tag for the storage account."
+  type        = string
+}
 
