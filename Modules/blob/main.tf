35f9489751efc343a8753e9eb319e8232eb95398
priyanka09602 <priyanka096.pradhan@gmail.com>
2024-08-06 20:42:27 +0530

    Update main.tf

--- Modules/blob/main.tf
+++ Modules/blob/main.tf
@@ -6,6 +6,7 @@ resource "azurerm_resource_group" "idc-hack-rg" {
   name     = var.resource_group_name
   location = var.location
 }
+
 resource "azurerm_storage_account" "idc-hack-sa" {
   name                     = var.storage_account_name
   resource_group_name      = var.resource_group_name
@@ -20,7 +21,7 @@ resource "azurerm_storage_container" "idc-hack-sc" {
   container_access_type = var.container_access_type
 }
 
-resource "azurerm_storage_blob" "example" {
+resource "azurerm_storage_blob" "idc-hack-sb" {
   name                   = "my-awesome-content.zip"
   storage_account_name   = azurerm_storage_account.example.name
   storage_container_name = azurerm_storage_container.example.name
